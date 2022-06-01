3996663080488623701789e9ec1c3fb3f3853975
Harald Schmidt <scharsc@googlemail.com>
2022-06-01 13:27:46 +0200

    tic tac toe works

--- src/index.js
+++ src/index.js
@@ -1,8 +1,8 @@
 import { createRoot } from 'react-dom/client';
 import React from 'react';
 import ReactDOM from 'react-dom';
-import Home from './components/Home';
+import Game from './components/Home';
 
 const container = document.getElementById('root');
 const root = createRoot(container); // createRoot(container!) if you use TypeScript
-root.render(<Home />);
+root.render(<Game />);
