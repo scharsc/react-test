42e87207f5059b03f374ce6a7c43ecfd909ddfd7
Harald Schmidt <scharsc@googlemail.com>
2022-06-03 13:51:07 +0200

    Mov BoardChecking to own namespace

--- src/components/Home.tsx
+++ src/components/Home.tsx
@@ -17,21 +17,22 @@ const Square: React.FC<SquareProps> = (props: SquareProps) =>
     );
 }
 
+type BoardArray = XorO[][];
+
 interface BoardState
 {
-    squares: XorO[][];
+    squares: BoardArray;
     nextState: XorO;
 }
 
-class Board extends React.Component<{},BoardState> {
-    constructor()
+class Board extends React.Component<BoardState,{}> {
+    constructor(props: BoardState)
     {
-        super({});
-        this.state = { squares: Array<XorO[]>(3).fill(Array<XorO>(3).fill("")), nextState: "X" }
+        super(props);
     }
 
     renderSquare(row: number, column: number) {
-        return <Square xorO={this.state.squares[row][column]} onClick={ () => this.onCardClicked(row, column) }/>;
+        return <Square xorO={this.props.squares[row][column]} onClick={ () => this.onCardClicked(row, column) }/>;
     }
 
     render() {
@@ -39,7 +40,7 @@ class Board extends React.Component<{},BoardState> {
 
         const status =  winner != "" ?  
                             "Winner is:" + winner :  
-                            'Next player: ' + this.state.nextState;
+                            'Next player: ' + this.props.nextState;
         return (
             <div>
                 <div className="status">{status}</div>
@@ -60,68 +61,6 @@ class Board extends React.Component<{},BoardState> {
                 </div>
             </div>
         );
-    }
-
-    private isThereAWinner(): XorO 
-    {
-        const val = [this.isThereAWinnerInRows(),
-        this.isThereAWinnerInColumns(),
-        this.isThereAWinnerInDiagOne(),
-        this.isThereAWinnerInDiagTwo()].find( e => e != "" );
-        return val == undefined? "" : val;
-    }
-
-    private isThereAWinnerInDiagTwo(): XorO {
-        const diagTwoCoordinatesFunc = (i: number): [number, number] => {
-            return [i, 2-i];
-        }
-        return this.isThereAWinnerInCoordinates(diagTwoCoordinatesFunc);
-    }
-
-    private isThereAWinnerInDiagOne(): XorO {
-        const diagOneCoordinatesFunc = (i: number): [number, number] => {
-            return [i,i];
-        }
-        return this.isThereAWinnerInCoordinates( diagOneCoordinatesFunc );
-    }
-
-    private isThereAWinnerInRows(): XorO {
-        const getRowIndicesFuncForColumn = (row: number): (i: number) => [number, number] => {
-            return (i: number) => [i, row];
-        }
-        for (let column = 0; column < 3; ++column) {
-            const value = this.isThereAWinnerInCoordinates(getRowIndicesFuncForColumn(column));
-            if (value != "")
-                return value;
-        }
-        return "";
-    }
-
-    private isThereAWinnerInColumns(): XorO
-    {
-        const getColumnIndicesFuncForRow = (column: number): (i: number) => [number, number] => {
-            return (i: number) => [column, i];
-        }
-        for (let row = 0; row < 3; ++row) {
-            const value = this.isThereAWinnerInCoordinates(getColumnIndicesFuncForRow(row));
-            if (value != "")
-                return value;
-        }
-        return "";
-    }
-
-    private isThereAWinnerInCoordinates(partialCoordinatesFunc: (i: number) => [number, number] ): XorO 
-    {
-        const valueAtIndex = ( i: number ) =>{ 
-            const [column, row] = partialCoordinatesFunc(i);
-            return this.state.squares[column][row]; 
-        };
-        let value = valueAtIndex(0);
-        for( let i = 1; i<3; ++i )
-            if( valueAtIndex(i) != value )
-                return "";
-    
-        return value;
     }
 
     private onCardClicked( row: number, column: number )
@@ -149,7 +88,82 @@ class Board extends React.Component<{},BoardState> {
     }
 }
 
-class Game extends React.Component {
+namespace BoardStateChecker
+{
+    function isThereAWinner(boardarray: BoardArray): XorO
+    {
+        const val = [isThereAWinnerInRows(boardarray),
+            isThereAWinnerInColumns(boardarray),
+            isThereAWinnerInDiagOne(boardarray),
+            isThereAWinnerInDiagTwo(boardarray)].find(e => e != "");
+        return val == undefined ? "" : val;
+    }
+
+    function isThereAWinnerInDiagTwo(boardarray: BoardArray): XorO {
+        const diagTwoCoordinatesFunc = (i: number): [number, number] => {
+            return [i, 2 - i];
+        }
+        return isThereAWinnerInCoordinates(boardarray,diagTwoCoordinatesFunc);
+    }
+
+    function isThereAWinnerInDiagOne(boardarray: BoardArray): XorO {
+        const diagOneCoordinatesFunc = (i: number): [number, number] => {
+            return [i, i];
+        }
+        return isThereAWinnerInCoordinates(boardarray, diagOneCoordinatesFunc);
+    }
+
+    function isThereAWinnerInRows(boardarray: BoardArray): XorO {
+        const getRowIndicesFuncForColumn = (row: number): (i: number) => [number, number] => {
+            return (i: number) => [i, row];
+        }
+        for (let column = 0; column < 3; ++column) {
+            const value = isThereAWinnerInCoordinates(boardarray,getRowIndicesFuncForColumn(column));
+            if (value != "")
+                return value;
+        }
+        return "";
+    }
+
+    function isThereAWinnerInColumns(boardarray: BoardArray): XorO
+    {
+        const getColumnIndicesFuncForRow = (column: number): (i: number) => [number, number] => {
+            return (i: number) => [column, i];
+        }
+        for (let row = 0; row < 3; ++row) {
+            const value = isThereAWinnerInCoordinates(boardarray,getColumnIndicesFuncForRow(row));
+            if (value != "")
+                return value;
+        }
+        return "";
+    }
+
+    function isThereAWinnerInCoordinates(boardarray: BoardArray,partialCoordinatesFunc: (i: number) => [number, number]): XorO
+    {
+        const valueAtIndex = (i: number) => {
+            const [column, row] = partialCoordinatesFunc(i);
+            return boardarray[column][row];
+        };
+        let value = valueAtIndex(0);
+        for (let i = 1; i < 3; ++i)
+            if (valueAtIndex(i) != value)
+                return "";
+
+        return value;
+    }
+}
+
+interface GameState
+{
+    boardHistory: BoardState[];
+}
+
+class Game extends React.Component<{}, GameState > {
+    constructor() {
+        super({});
+        const initBoard: BoardState = { squares: Array<XorO[]>(3).fill(Array<XorO>(3).fill("")), nextState: "X" };
+        this.state = { boardHistory: [ initBoard ] };
+    }
     render() {
         return (
             <div className="game">
@@ -158,7 +172,9 @@ class Game extends React.Component {
                 </div>
                 <div className="game-info">
                     <div>{/* status */}</div>
-                    <ol>{/* TODO */}</ol>
+                    <ol>
+                        <li>fasdf</li>
+                    </ol>
                 </div>
             </div>
         );
