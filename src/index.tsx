7c57a1d1499c71ce229ecd3be31960f8e45c8472
Harald Schmidt <scharsc@googlemail.com>
2022-06-14 10:39:39 +0200

    Use R3F

--- src/index.tsx
+++ src/index.tsx
@@ -1,12 +1,17 @@
 import React from "react";
+import { createRoot } from 'react-dom/client'
 import ReactDOM from "react-dom";
 import Game from './components/Home';
 import "./mystyle.css"
+import App from './components/App'
+
 // const App = () => (
 //     <h1>My React and TypeScript App!</h1>
 // );
 
-ReactDOM.render(
-    <Game />,
-    document.getElementById("root")
-);
+createRoot(document.getElementById('root')).render(<App />)
+
+// ReactDOM.render(
+//     <Game />,
+//     document.getElementById("root")
+// );
